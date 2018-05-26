14d305154c265f8977b23cc72fd1dd826e26fd18
Nicholas Luedtke <nicholas.luedtke@uwalumni.com>
2018-05-26 18:54:14 -0400

    add a maxwidth to avoid taking up all of one continent

--- geoplot.py
+++ geoplot.py
@@ -45,13 +45,14 @@ def main(args):
     m.drawcoastlines()
     m.fillcontinents()
     m.drawcountries()
+    maxwidth = 10
 
     for evt in events:
         ipl = rd.get(evt)
         lat = ipl['location']['latitude']
         long = ipl['location']['longitude']
         m.plot(long, lat, 'ro', latlon=True, zorder=3,
-               markersize=events[evt] / 2)
+               markersize=min(events[evt] / 3, maxwidth))
 
     m.plot(dlon, dlat, 'go', latlon=True, zorder=2, markersize=4)
     geolite2.close()
